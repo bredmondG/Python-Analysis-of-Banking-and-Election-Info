1a20dfe602432cc91025993c97c7beb4e3f1291c
Brendan Gold <brendangold@Brendans-MacBook-Air.local>
2019-06-17 18:39:44 -0600

    needs tweaks

---
+++ PyPoll/main.py
@@ -0,0 +1,20 @@
+import csv
+
+from tqdm import tqdm
+
+from collections import counter
+
+candidates = set()
+
+with open(,"r") as f:
+    readerr = csv.reader
+    
+    
+    
+    
+votes = {"Trump": 100, "Biden": 97, "AOC": 102}
+
+votes["Biden"] += 1
+votes
+
+
