25beb37f653f7ba4f35688aaaf05a79ee6c43965
Brendan Gold <brendangold@Brendans-MacBook-Air.local>
2019-06-17 19:36:45 -0600

    update to view in terminal

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,20 +1,33 @@
 import csv
 
-from tqdm import tqdm
+#from tqdm import tqdm
 
-from collections import counter
+row_count = 0
+candidates = {}
 
-candidates = set()
+def candidate_count(row):
+    if  candidates.get(row[2]) == None:
+        candidates[row[2]] = 1
+    else:
+        candidates[row[2]] += 1
 
-with open(,"r") as f:
-    readerr = csv.reader
+with open("/Users/brendangold/DENVDEN201905DATA4/Homework/3 Python  6-18/PyPoll/Resources/election_data.csv", "r") as f:
+    reader = csv.reader(f)
     
+    next(reader)
     
-    
-    
-votes = {"Trump": 100, "Biden": 97, "AOC": 102}
+    for row in reader:
+        candidate_count(row)
+        row_count += 1
 
-votes["Biden"] += 1
-votes
+def winner():
+    value = max(list(candidates.values()))
+    for key, val in candidates.items():
+        if val == value:
+            return(key, val)
 
+print("Total Votes:", row_count)
+for key, val in candidates.items():
+    print(key, ":", val, (round(val / row_count * 100)), "%")
+print("Winner:", winner())
 
